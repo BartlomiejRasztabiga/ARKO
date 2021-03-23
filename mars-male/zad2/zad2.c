35779a26a96c28d273eccd11d8bd949cb3c454b2
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-03-24 00:36:10 +0100

    Add zad2 - C

---
+++ mars-male/zad2/zad2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define BUF_SIZE 50
+
+int main() {
+    char buf[BUF_SIZE];
+    scanf("%s", buf);
+
+    int largest = 0;
+    int current_size = 0;
+    int largest_start = 0;
+    int largest_end = 0;
+
+    int i = 0;
+    for (; i < BUF_SIZE; ++i) {
+        char current = buf[i];
+
+        if (current == '\0') {
+            break;
+        }
+
+        if (current >= '0' && current <= '9') {
+            current_size++;
+        } else {
+            if (current_size > largest) {
+                largest = current_size;
+                largest_end = i;
+                largest_start = i - current_size;
+            }
+            current_size = 0;
+        }
+    }
+
+    if (current_size > largest) {
+        largest_end = i;
+        largest_start = i - current_size;
+    }
+
+    for (int j = largest_start; j < largest_end; ++j) {
+        printf("%c", buf[j]);
+    }
+
+    return 0;
+}
