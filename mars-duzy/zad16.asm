0af34da94313837cea6f41aa2d89361485d05116
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-04-03 19:41:49 +0200

    delete stack operations from some leaves

--- mars-duzy/zad16.asm
+++ mars-duzy/zad16.asm
@@ -539,9 +539,8 @@ copy_src_return:
 
 	jr	$ra				# return new free char address of destination
 	
-# ============================================================================  	
-# TODO: delete if not used
-# copy_src_range_to_dest
+# ============================================================================
+# copy_src_range_to_dest (LEAF)
 # description: 
 #	copies src string from given range to dest
 # arguments:
@@ -549,97 +548,46 @@ copy_src_return:
 #	$a1 - src address end (won't be copied)
 #	$a2 - dest address
 # variables:
-#	$s0 - src address
-#	$s1 - src address end
-#	$s2 - dest address
-#	$s3 - current char
+#	$t0 - current char
 # returns:
 #	$v0 - address of next free char at destination
-copy_src_range_to_dest:				# takes addresses of src and destination as params
-	sub	$sp, $sp, 4
-	sw	$ra, 4($sp)			# push $ra
-	sub	$sp, $sp, 4
-	sw	$s0, 4($sp)			# push $s0
-	sub	$sp, $sp, 4
-	sw 	$s1, 4($sp)			# push $s1
-	sub	$sp, $sp, 4
-	sw 	$s2, 4($sp)			# push $s2
-	sub	$sp, $sp, 4
-	sw 	$s3, 4($sp)			# push $s3
-
-	move	$s0, $a0			# address of src
-	move	$s1, $a1			# address of src ending
-	move	$s2, $a2			# address of destination
-copy_src_range_loop:
-	lb	$s3, ($s0)			# store buffer char in $t7
-	beq	$s0, $s1, copy_src_range_return	# if met end of range, return
+copy_src_range_to_dest:
+	lb	$t0, ($a0)			# store buffer char in $t7
+	beq	$a0, $a1, copy_src_range_return	# if met end of range, return
 	
-	sb	$s3, ($s2)			# else, store src char at destination address
-	addiu	$s0, $s0, 1			# next src char
-	addiu	$s2, $s2, 1			# next destination char
-	j copy_src_range_loop			# if not met end of string, repeat loop
+	sb	$t0, ($a2)			# else, store src char at destination address
+	addiu	$a0, $a0, 1			# next src char
+	addiu	$a2, $a2, 1			# next destination char
+	j copy_src_range_to_dest		# if not met end of string, repeat loop
 copy_src_range_return:
-	move	$v0, $s2
-	
-	lw	$s3, 4($sp)			# pop $s3
-	add	$sp, $sp, 4	
-	lw	$s2, 4($sp)			# pop $s2
-	add	$sp, $sp, 4	
-	lw	$s1, 4($sp)			# pop $s1
-	add	$sp, $sp, 4			
-	lw	$s0, 4($sp)			# pop $s0
-	add	$sp, $sp, 4			
-	lw	$ra, 4($sp)			# pop $ra
-	add	$sp, $sp, 4
-
+	move	$v0, $a2
 	jr	$ra				# return new free char address of destination
 
 # ============================================================================
-# str_len
+# str_len (LEAF)
 # description:
 #	returns length of string in memory
 # arguments:
 #	$a0 - string address
 # variables:
-#	$s0 - string address
-#	$s1 - length
-# 	$s2 - current char
+#	$t0 - string address
+#	$t1 - length
+# 	$t2 - current char
 # returns:
 #	$v0 - length of string in bytes
 str_len:
-	sub	$sp, $sp, 4
-	sw	$ra, 4($sp)			# push $ra
-	sub	$sp, $sp, 4
-	sw	$s0, 4($sp)			# push $s0
-	sub	$sp, $sp, 4
-	sw 	$s1, 4($sp)			# push $s1
-	sub	$sp, $sp, 4
-	sw 	$s2, 4($sp)			# push $s2
-
-	move 	$s0, $a0			# string address
-	li	$s1, 0				# length = 0
+	move 	$t0, $a0			# string address
+	li	$t1, 0				# length = 0
 str_len_loop:
-	lb	$s2, ($s0)			# get current char
-	beqz	$s2, str_len_return		# if NULL, goto str_len_return
-
-	addiu	$s1, $s1, 1			# if not NULL, length++
+	lb	$t2, ($t0)			# get current char
+	beqz	$t2, str_len_return		# if NULL, goto str_len_return
+	addiu	$t1, $t1, 1			# if not NULL, length++
 str_len_next_char:
-	addiu	$s0, $s0, 1			# next char
+	addiu	$t0, $t0, 1			# next char
 	j 	str_len_loop
-
 str_len_return:
-	move	$v0, $s1
-
-	lw	$s2, 4($sp)			# pop $s2
-	add	$sp, $sp, 4
-	lw	$s1, 4($sp)			# pop $s1
-	add	$sp, $sp, 4
-	lw	$s0, 4($sp)			# pop $s0
-	add	$sp, $sp, 4
-	lw	$ra, 4($sp)			# pop $ra
-	add	$sp, $sp, 4
-
-	jr	$ra				# return
+	move	$v0, $t1
+	jr	$ra
 	
 # ============================================================================
 # min (LEAF)
@@ -652,18 +600,12 @@ str_len_return:
 # returns:
 #	$v0 - less of two ints
 min:
-	sub	$sp, $sp, 4
-	sw	$ra, 4($sp)			# push $ra
-
 	blt	$a0, $a1, min_first		# if $a0 < $a1, goto min_first
 	move	$v0, $a1			# return $a1
 	j 	min_return			# TODO: refactor?
 min_first:
 	move	$v0, $a0			# return $a0
 min_return:
-	lw	$ra, 4($sp)			# pop $ra
-	add	$sp, $sp, 4
-
 	jr	$ra				# return
 	
 # ============================================================================
@@ -680,6 +622,8 @@ min_return:
 #	$t3 - current label's char
 #	$t4 - current word's char
 #	$t5 - pointer for current label section
+#	$t6 - flag if label has ended
+#	$t7 - flag if word has ended
 # returns:
 #	$v0 - line number for symbol if word is a defined symbol, -1 if not defined
 get_symbol_for_word:	
