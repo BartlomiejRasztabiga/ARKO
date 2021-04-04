11c568ff90a6b7e737363bf31c174e727038ce2f
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-04-04 20:11:55 +0200

    Add more tests

--- mars-duzy/tests/test.sh
+++ mars-duzy/tests/test.sh
@@ -4,8 +4,17 @@ java -jar mars.jar nc ../zad16.asm
 
 if cmp --silent -- "output.txt" "expected_output.txt";
 then
-    exit 0
+    echo "test 1 passed"
 else
-    echo "invalid file output"
+    echo "test 1 failed"
+    exit 1
+fi
+
+
+if cmp --silent -- "output1.txt" "expected_output1.txt";
+then
+    echo "test 2 passed"
+else
+    echo "test 2 failed"
     exit 1
 fi
