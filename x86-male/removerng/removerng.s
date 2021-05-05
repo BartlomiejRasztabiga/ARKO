0178d0ec00b4c2eebab8d9f7fd590e4169aab7b4
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-05-05 13:48:07 +0200

    WIP: removerng segfault

--- x86-male/removerng/removerng.s
+++ x86-male/removerng/removerng.s
@@ -1,56 +1,71 @@
         section .text
         global  removerng
+
+
 removerng:
+; arguments:
+;	    char *s - pointer to an input string
+;       char  a - start of char range
+;       char  b - end of char range
+; returns:
+;	    char *  - address of input string
+; local variables:
+;       char *source        - ebp-4
+;       char *dest          - ebp-8
+;       char currentChar    - ebp-9
+
 ; prologue
-        ;push    ebp
-        ;mov     ebp, esp
-        enter   8, 0                    ; equivalent of two instructions above
+        push    ebp
+        mov     ebp, esp
+        sub     esp, 9
 
 ; push saved registers
-        push    ebx
-        push    esi
-        push    edi
+        ;push    ebx
+        ;push    esi
+        ;push    edi
 
 ; function body
         mov     eax, DWORD [ebp+8]      ; s argument
         mov     DWORD [ebp-4], eax      ; source = s
         mov     DWORD [ebp-8], eax      ; dest = s
 removerng_next_char:
-        mov     eax, DWORD [ebp-4]      ; get address of source
-        movzx   eax, BYTE [eax]         ; read char
-        inc     DWORD [ebp-4]           ; source++
+        mov     eax, DWORD [ebp-4]      ; get address of source to eax
+        movzx   eax, BYTE [eax]         ; read char to eax
+        mov     BYTE [ebp-9], al        ; char currentChar = *source;
 
-        cmp     eax, 0                  ; check if char is NULL
-        jz      removerng_ret           ; if char is NULL, goto removerng_ret
+        add     DWORD [ebp-4], 1        ; source++ (next char)
 
-        ; TODO: revert condition - currentChar >= a && currentChar <= b
+        cmp     BYTE [ebp-9], 0         ; check if currentChar is NULL
+        je      removerng_ret           ; if char is NULL, goto removerng_ret
 
-        cmp     al, BYTE [ebp+12]       ; compare current char with A
-        jl      removerng_write_char    ; if less, write that char
+        movzx   eax, BYTE [ebp-9]       ; eax = currentChar
+        cmp     eax, [ebp+12]           ; compare current char with A
+        jl      removerng_write_char    ; if currentChar < A, write that char
 
-        cmp     al, BYTE [ebp+16]       ; compare current char with B
-        jg      removerng_write_char    ; if greater, write that char
-
-        jmp     removerng_next_char     ; read next char
+        movzx   eax, BYTE [ebp-9]       ; eax = currentChar
+        cmp     eax, [ebp+16]           ; compare current char with B
+        jle     removerng_next_char     ; if currentChar <= B, go back to loop
+                                        ; if currentChar > B, write that char
 removerng_write_char:
-        mov     edx, DWORD [ebp-8]      ; edx = dest
-        movzx   edx, BYTE al            ; store current char to dest
-        inc     DWORD [ebp-8]           ; dest++
+        mov     eax, DWORD [ebp-8]      ; eax = dest
+        mov     [eax], edx              ; *dest = currentChar;
+
+        add     DWORD [ebp-8], 1        ; dest++
 
         jmp     removerng_next_char     ; read next char
 removerng_ret:
         mov     eax, DWORD [ebp-8]      ; eax = dest
-        mov     BYTE [eax], 0           ; store NULL at dest
+        mov     BYTE [eax], 0           ; store NULL at dest, TODO: change to xor
+
+        mov     eax, DWORD [ebp+8]      ; return s (in eax)
 
 ; pop saved registers
-        pop     edi
-        pop     esi
-        pop     ebx
+        ;pop     edi
+        ;pop     esi
+        ;pop     ebx
 
 ; epilogue
-        ;mov     esp, ebp
-        ;pop     ebp
-        leave                           ; equivalent of two instructions above
+        leave                           ; restore esp and ebp
         ret
 
 strlen:
