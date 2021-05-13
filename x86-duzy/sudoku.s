1292f94bf1130259f0d17848e789cef6acf25b0c
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-05-13 17:55:39 +0200

    WIP: sudoku in ASM

--- x86-duzy/sudoku.s
+++ x86-duzy/sudoku.s
@@ -10,18 +10,110 @@
 ;   - unsigned int row  ebp+12
 ;   - unsigned int col  ebp+16
 ; variables:
-;   -
+;   - char num          ebp-4
 ; returns:
 ;   - eax: 1 if found solution, 0 otherwise
+; TODO: Replace mov 0 with xor x,x
 sudoku:
         push    ebp
         mov     ebp, esp
         sub     esp, 24
 
+; TODO: rearrange jumps?
+        cmp     DWORD [ebp+16], 9               ; test if col == 9
+        jne     .sudoku_not_last_col            ; if not equal, goto .sudoku_not_last_col
 
-;        jne
+        cmp     DWORD [ebp+12], 8               ; test if row == 8
+        jne     .sudoku_not_last_row            ; if not equal, goto .sudoku_not_last_row
 
-.sudoku_not_finished:
+        mov     eax, 1
+        jmp     .sudoku_return                  ; if last row, return 1
+.sudoku_not_last_row:
+        add     DWORD [ebp+12], 1               ; row++
+        mov     DWORD [ebp+16], 0               ; col = 0
+.sudoku_not_last_col:
+        mov     edx, DWORD [ebp+12]             ; edx = row
+        lea     edx, [edx, edx*8]               ; edx = 9 * x
+        mov     eax, DWORD [ebp+8]              ; eax = pointer to grid
+        add     edx, eax                        ; edx = pointer to grid's row
+        mov     eax, DWORD [ebp+16]             ; eax = int col;
+        add     eax, edx                        ; eax = pointer to grid's tile at [x][col]
+        movzx   eax, BYTE [eax]                 ; eax = char from grid' tile at [x][col]
+        cmp     al, '#'                         ; test if grid[row][col] == '#' - no value at tile
+        je      .sudoku_find_value              ; if equal, goto .sudoku_find_value
+
+                                                ; if not equal, return sudoku(grid, row, col + 1);
+        mov     eax, DWORD [ebp+16]             ; eax = col
+        add     eax, 1                          ; eax = col + 1
+        sub     esp, 4                          ; stack has to be aligned to 16, 3*4 + 4 = 16
+        push    eax                             ; push (col+1)
+        push    DWORD [ebp+12]                  ; push row
+        push    DWORD [ebp+8]                   ; push grid
+        call    sudoku                          ; call sudoku
+        add     esp, 16                         ; free stack
+        jmp     .sudoku_return                  ; return eax (return value from sudoku(grid, row, col + 1))
+; TODO: Rearrange jumps
+.sudoku_find_value:
+        mov     BYTE [ebp-4], '1'               ; num = '1'
+        jmp     .sudoku_find_value_loop_condition
+.sudoku_find_value_loop:
+        movsx   eax, BYTE [ebp-9]
+        push    eax
+        push    DWORD [ebp+16]
+        push    DWORD [ebp+12]
+        push    DWORD [ebp+8]
+        call    isSafe
+        add     esp, 16
+        cmp     eax, 1
+        jne     .sudoku_find_value_loop_next_num
+
+        mov     edx, DWORD [ebp+12]
+        mov     eax, edx
+        sal     eax, 3
+        add     edx, eax
+        mov     eax, DWORD [ebp+8]
+        add     edx, eax
+        mov     eax, DWORD [ebp+16]
+        add     edx, eax
+        movzx   eax, BYTE [ebp-9]
+        mov     BYTE [edx], al
+
+        mov     eax, DWORD [ebp+16]
+        add     eax, 1
+        sub     esp, 4
+        push    eax
+        push    DWORD [ebp+12]
+        push    DWORD [ebp+8]
+        call    sudoku
+        add     esp, 16
+        cmp     eax, 1
+        jne     .sudoku_find_value_loop_next_num
+
+        mov     eax, 1
+        jmp     .sudoku_return
+.sudoku_find_value_loop_next_num:
+        mov     edx, DWORD [ebp+12]
+        mov     eax, edx
+        sal     eax, 3
+        add     edx, eax
+        mov     eax, DWORD [ebp+8]
+        add     edx, eax
+        mov     eax, DWORD [ebp+16]
+        add     eax, edx
+        mov     BYTE [eax], 35
+
+                                                ; num++, try next char
+        movzx   eax, BYTE [ebp-9]
+        add     eax, 1
+        mov     BYTE [ebp-9], al
+
+.sudoku_find_value_loop_condition:
+        cmp     BYTE [ebp-4], '9'               ; test if num <= '9'
+        jle     .sudoku_find_value_loop         ; if true, goto loop
+        mov     eax, 0                          ; return 0
+.sudoku_return:
+        leave
+        ret
 
 
 ; ============================================================================
