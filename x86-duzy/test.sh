c464dd2123f829b1048f629f9c07a1c60a3cb3b7
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-05-17 13:51:38 +0200

    Add more tests

--- x86-duzy/test.sh
+++ x86-duzy/test.sh
@@ -12,3 +12,26 @@ else
     echo "test 1 failed"
     exit 1
 fi
+
+# Test 2
+./main < tests/input2.txt > output.txt
+
+if cmp --silent -- "output.txt" "tests/output2.txt";
+then
+    echo "test 2 passed"
+else
+    echo "test 2 failed"
+    exit 1
+fi
+
+# Test 3
+./main < tests/input3.txt > output.txt
+
+if cmp --silent -- "output.txt" "tests/output3.txt";
+then
+    echo "test 3 passed"
+else
+    echo "test 3 failed"
+    echo "test 3 failed"
+    exit 1
+fi
